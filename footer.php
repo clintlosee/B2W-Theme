9dfe83a8575184e5c81e4ff7c9aa37556188e380
Clint Losee <clint.losee@gmail.com>
2015-09-07 10:52:41 -0600

    Added footer content to footer.php. Added custom home page-home.php.

--- footer.php
+++ footer.php
@@ -13,6 +13,78 @@
 
 <?php wp_footer(); ?>
 
+	<!-- SIGN UP SECTION
+	================================ -->
+	<section id="signup" data-type="background" data-speed="4">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-6 col-sm-offset-3">
+					<h2>Are you ready to to take your coding skills to the <strong>next level</strong>?</h2>
+					<p><a href="#" class="btn btn-lg btn-block btn-success ">Yes, sign me up!</a></p>
+				</div> <!-- end col -->
+			</div> <!-- end row -->
+		</div> <!-- end container -->
+	</section> <!-- end sign-up -->
+
+	<!-- FOOTER
+	================================ -->
+	<footer>
+		<div class="container">
+			<div class="col-sm-3">
+				<p><a href="/"><img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/logo.png" alt="Bootstrap to WordPress"></a></p>
+			</div> <!-- end col -->
+
+			<div class="col-sm-6">
+				<nav>
+					<ul class="list-unstyled list-inline">
+						<li><a href="/">Home</a></li>
+						<li><a href="">Blog</a></li>
+						<li><a href="">Resources</a></li>
+						<li><a href="">Contact</a></li>
+						<li class="signup-link"><a href="">Sign up now</a></li>
+					</ul> <!-- end list-unstyled -->
+				</nav> <!-- end nav -->
+			</div> <!-- end col -->
+			<div class="col-sm-3">
+				<p class="pull-right">&copy; 2015 Clint Losee</p>
+			</div> <!-- end col -->
+		</div> <!-- end container -->
+	</footer> <!-- end footer -->
+
+	<!-- MODAL SECTION
+	================================ -->
+	<div class="modal fade" id="myModal">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+					<h4 class="modal-title" id="myModalLabel"><i class="fa fa-envelope"></i>Subscribe</h4>
+				</div> <!-- end modal-header -->
+
+				<div class="modal-body">
+					<p>Simply enter your name and e-mail to stay up-to-date on the latest news and information!</p>
+
+					<form class="form-inline" role="form">
+						<div class="form-group">
+							<label class="sr-only" for="subscribe-name">Your first name</label>
+							<input type="text" class="form-control" id="subscribe-name" placeholder="Your first name">
+						</div> <!-- end form-group -->
+						<div class="form-group">
+							<label class="sr-only" for="subscribe-email">and your e-mail</label>
+							<input type="text" class="form-control" id="subscribe-email" placeholder="and your e-mail">
+						</div> <!-- end form-group -->
+
+						<input type="submit" class="btn btn-danger" value="Subscribe">
+					</form>
+
+					<hr>
+
+					<p><small>By providing your e-mail you consent to receiving promotional e-mails &amp; newsletters. <br>No spam. Just good stuff. We respect your privacy &amp; you may unsubscribe at any time.</small></p>
+				</div> <!-- end modal-body -->
+			</div> <!-- end modal-content -->
+		</div> <!-- end modal-dialog -->
+	</div> <!-- end modal -->
+
 	<!-- JAVASCRIPT -->
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 	<script type="text/javascript">
---
+++ page-home.php
@@ -0,0 +1,324 @@
+<?php
+/*
+    Template Name: Home Page
+*/
+
+get_header(); ?>
+
+	<!-- HERO
+	================================ -->
+	<section id="hero" data-type="background" data-speed="5">
+		<article>
+			<div class="container clearfix">
+				<div class="row">
+					<div class="col-sm-5">
+						<img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/logo-badge.png" alt="Bootstrap to WordPress" class="logo">
+					</div> <!-- end col -->
+
+					<div class="col-sm-7">
+						<h1>Bootstrap to WordPress</h1>
+						<p class="lead">Earn an Extra $1k - $5k a Month by Learning to Code Your Very Own Responsive &amp; Custom WordPress Websites with Bootstrap</p>
+
+						<div id="price-timeline">
+							<div class="price active">
+								<h4>Pre-Launch Price <small>Ends soon!</small></h4>
+								<span>$149</span>
+							</div> <!-- end price -->
+
+							<div class="price">
+								<h4>Launch Price <small>Coming soon!</small></h4>
+								<span>$299</span>
+							</div> <!-- end price -->
+
+							<div class="price">
+								<h4>Final Price <small>Coming soon!</small></h4>
+								<span>$399</span>
+							</div> <!-- end price -->
+
+							<p><a class="btn btn-lg btn-danger" href="/" role="button">Enroll &raquo;</a></p>
+						</div>
+					</div> <!-- end col -->
+				</div> <!-- end row -->
+			</div> <!-- end container -->
+		</article>
+	</section> <!-- end hero -->
+
+	<!-- OPT IN SECTION
+	================================ -->
+	<section id="optin">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-8">
+					<p class="lead"><strong>Subscribe to our mailing list.</strong> Keep updated on what's new.</p>
+				</div> <!-- end col -->
+
+				<div class="col-sm-4">
+					<button class="btn btn-success btn-lg btn-block" data-toggle="modal" data-target="#myModal"> Click here to subscribe</button>
+				</div> <!-- end col -->
+			</div> <!-- end row -->
+		</div> <!-- end container -->
+	</section> <!-- end optin -->
+
+	<!-- BOOST YOUR INCOME
+	================================ -->
+	<section id="boost-income">
+		<div class="container">
+			<div class="section-header">
+				<img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/icon-boost.png" alt="Chart">
+				<h2>How You Can Boost Your Income</h2>
+			</div> <!-- end section-header -->
+
+			<p class="lead">Whether you're a freelance designer, entrepreneur, employee for a company, code hobbyist, or looking for a new career — this course gives you an immensely valuable skill that will enable you to either:</p>
+
+			<div class="row">
+				<div class="col-sm-6">
+					<h3>Make money on the side</h3>
+					<p>So you can save up for that Hawaiian vacation you've been wanting, help pay off your debt, your car, your mortgage, or simply just to have bonus cash laying around.</p>
+				</div> <!-- end col -->
+				<div class="col-sm-6">
+					<h3>Create a full-time income</h3>
+					<p>WordPress developers have options. Many developers make a generous living off of creating custom WordPress themes and selling them on websites like ThemeForest. Freelance designers and developers can also take on WordPress projects and make an extra $1,000 - $5,000+ per month.</p>
+				</div> <!-- end col -->
+			</div> <!-- end row -->
+		</div> <!-- end container -->
+	</section>
+
+	<!-- WHO BENEFITS
+	================================ -->
+	<section id="who-benefits">
+		<div class="container">
+			<div class="section-header">
+				<img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/icon-pad.png" alt="Pad and pencil">
+				<h2>Who Should Take This Course?</h2>
+			</div> <!-- end section-header -->
+
+			<div class="row">
+				<div class="col-sm-8 col-sm-offset-2">
+					<h3>Graphics &amp; Web Designers</h3>
+					<p>Graphic designers are extremely talented, but ask them to code their designs and they'll freeze up! This leaves them with no other choice but to hire a web developer. Any professional graphic designers knows web developers can be expensive.</p>
+					<p>If you're a designer, learning to code your own WordPress websites can change your business entirely! Now, not only are you a great designer, but you're a skillful developer, too! This puts you in a position to make an extra $1,000 – $5,000 per project.</p>
+
+					<h3>Entrepreneurs</h3>
+					<p>Entrepreneurs have big dreams, and in many cases, shoestring budgets. In order to survive in the cut-throat world of the Startup company, it's a necessity to have a world-class website. However, world-class websites come with a large price tag.</p>
+					<p>If you can learn how to build a high-quality startup website by yourself, then you’ve just saved yourself a lot of cash, <strong>tens of thousands of dollars in many cases.</strong></p>
+
+					<h3>Employees</h3>
+					<p>Any company knows the education &amp; training of their employees is key to a thriving team.
+					Depending on the type of company you work for, if you understand how to code, and can develop CMS driven websites, that gives you <strong>negotiating power for a better position, or a higher salary.</strong></p>
+
+					<h3>Code Hobbyists</h3>
+					<p>It's fun to learn challenging new skills. Code hobbyists can add dynamic websites to their arsenal of tools to play with — you can even sell WordPress themes and plugins for cash! The possibilities are truly endless.<p>
+
+					<h3>People Looking for a New Career</h3>
+					<p>Are you out of work? Looking for a more rewarding job? Desire a career that can allow you to work almost anywhere in the world? Becoming a Web Developer might be the answer for you.</p>
+					<p><strong>Web developers are paid well, anywhere from $33,000 to more than $105,000 per year.</strong> They get to work at amazing companies that are changing the world, or they enjoy the ability to start their own companies, become location-independent and work from home, from coffee shops, in an airplane, on the beach, or wherever they want!</p>
+				</div> <!-- end col -->
+			</div> <!-- end row -->
+		</div> <!-- end container -->
+	</section> <!-- end who-benefits -->
+
+	<!-- COURSE FEATURES
+	================================ -->
+	<section id="course-features">
+		<div class="container">
+			<div class="section-header">
+				<img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/icon-rocket.png" alt="Rocket">
+				<h2>Course Features</h2>
+			</div> <!-- end section-header -->
+
+			<div class="row">
+				<div class="col-sm-2">
+					<i class="ci ci-computer"></i>
+					<h4>Lifetime access to 80+ lectures</h4>
+				</div> <!-- end col -->
+				<div class="col-sm-2">
+					<i class="ci ci-watch"></i>
+					<h4>10+ hours of HD video content</h4>
+				</div> <!-- end col -->
+				<div class="col-sm-2">
+					<i class="ci ci-calendar"></i>
+					<h4>30-day money back guarantee</h4>
+				</div> <!-- end col -->
+				<div class="col-sm-2">
+					<i class="ci ci-community"></i>
+					<h4>Access to a community of like-minded students</h4>
+				</div> <!-- end col -->
+				<div class="col-sm-2">
+					<i class="ci ci-instructor"></i>
+					<h4>Direct access to the instructor</h4>
+				</div> <!-- end col -->
+				<div class="col-sm-2">
+					<i class="ci ci-device"></i>
+					<h4>Accessible content on your mobile devices</h4>
+				</div> <!-- end col -->
+			</div> <!-- end row -->
+		</div> <!-- end container -->
+	</section>
+
+	<!-- PROJECT FEATURES
+	================================ -->
+	<section id="project-features">
+		<div class="container">
+			<h2>Final Project Features</h2>
+			<p class="lead">Throughout this entire course, you work towards building an incredibly beautiful website. Want to see the website you are going to build? You're looking at it! The website you're using right now is the website you will have built entirely by yourself, by the end of this course.</p>
+
+			<div class="row">
+				<div class="col-sm-4">
+					<img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/icon-design.png" alt="Design">
+					<h3>Sexy &amp; Modern Design</h3>
+					<p>You get to work with a modern, professional quality design &amp; layout.</p>
+				</div> <!-- end col -->
+				<div class="col-sm-4">
+					<img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/icon-code.png" alt="Code">
+					<h3>Quality HTML5 &amp; CSS3</h3>
+					<p>You'll learn how hand-craft a stunning website with valid, semantic and beautiful HTML5 &amp; CSS3.</p>
+				</div> <!-- end col -->
+				<div class="col-sm-4">
+					<img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/icon-cms.png" alt="CMS">
+					<h3>Easy-to-Use CMS</h3>
+					<p>Allow your clients to easily update their websites by converting your static websites to dynamic websites, using WordPress.</p>
+				</div> <!-- end col -->
+			</div> <!-- end row -->
+		</div> <!-- end container -->
+	</section>
+
+	<!-- VIDEO FEATURETTE
+	================================ -->
+	<section id="video-feature">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-8 col-sm-offset-2">
+					<h2>Watch the Course Introduction</h2>
+					<iframe width="100%" height="415" src="https://www.youtube.com/embed/UfVB9u0-Dvs?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+				</div>
+			</div> <!-- end row -->
+		</div> <!-- end container -->
+	</section>
+
+	<!-- INSTRUCTOR
+	================================ -->
+	<section id="instructor">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-8 col-md-6">
+					<div class="row">
+						<div class="col-lg-8">
+							<h2>Your Instructor <small>Clint Losee</small></h2>
+						</div> <!-- end col -->
+						<div class="col-lg-4">
+							<a href="https://twitter.com" target="_blank" class="badge social twitter"><i class="fa fa-twitter"></i></a>
+							<a href="https://facebook.com" target="_blank" class="badge social facebook"><i class="fa fa-facebook"></i></a>
+							<a href="https://googleplus.com" target="_blank" class="badge social gplus"><i class="fa fa-google-plus"></i></a>
+						</div> <!-- end col -->
+					</div> <!-- end row -->
+
+					<p class="lead">A highly skilled professional, Clint Losee is a passionate and experienced web developer, blogger and digital entrepreneur.</p>
+
+					<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+
+					<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+
+					<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+
+					<hr>
+
+					<h3>The Numbers <small>They Don't Lie</small></h3>
+
+					<div class="row">
+						<div class="col-xs-4">
+							<div class="num">
+								<div class="num-content">
+									41,000+ <span>students</span>
+								</div> <!-- end num-content -->
+							</div> <!-- end num -->
+						</div> <!-- end col -->
+
+						<div class="col-xs-4">
+							<div class="num">
+								<div class="num-content">
+									560 <span>reviews</span>
+								</div> <!-- end num-content -->
+							</div> <!-- end num -->
+						</div> <!-- end col -->
+
+						<div class="col-xs-4">
+							<div class="num">
+								<div class="num-content">
+									4 <span>courses</span>
+								</div> <!-- end num-content -->
+							</div> <!-- end num -->
+						</div> <!-- end col -->
+					</div> <!-- end row -->
+
+				</div> <!-- end col -->
+			</div> <!-- end row -->
+		</div> <!-- end container -->
+	</section> <!-- end instructor -->
+
+	<!-- TESTIMONIALS
+	================================ -->
+	<section id="kudos">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-8 col-sm-offset-2">
+
+					<h2>What People Are Saying About Clint</h2>
+
+					<!-- TESTIMONIAL -->
+					<div class="row testimonial">
+						<div class="col-sm-4">
+							<img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/brennan.jpg" alt="Brennan">
+						</div> <!-- end col -->
+						<div class="col-sm-8">
+							<blockquote>
+								Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.
+								<cite>&mdash; Brennan, graduate of this course</cite>
+							</blockquote> <!-- end blockquote -->
+						</div> <!-- end col -->
+					</div> <!-- end row testimonial -->
+
+					<!-- TESTIMONIAL -->
+					<div class="row testimonial">
+						<div class="col-sm-4">
+							<img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/ben.png" alt="Illustration of a man with a moustache">
+						</div> <!-- end col -->
+						<div class="col-sm-8">
+							<blockquote>
+								Banjo tattooed sriracha polaroid, Banksy Marfa Neutra iPhone cray. Pug leggings sartorial, iPhone narwhal Pinterest Godard. Cardigan aesthetic next level typewriter pork belly Williamsburg American Apparel, YOLO chia pug. Ethical occupy keytar, leggings put a bird on it church-key before they sold out letterpress Thundercats ennui heirloom trust fund Truffaut. Narwhal leggings pork belly authentic viral. Quinoa Marfa fanny pack vegan cred. Organic gluten-free photo booth, single-origin coffee seitan Thundercats farm-to-table twee Portland fingerstache XOXO beard Intelligentsia flexitarian locavore.
+								<cite>&mdash; Ben, graduate of this other course</cite>
+							</blockquote> <!-- end blockquote -->
+						</div> <!-- end col -->
+					</div> <!-- end row testimonial -->
+
+					<!-- TESTIMONIAL -->
+					<div class="row testimonial">
+						<div class="col-sm-4">
+							<img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/aj.png" alt="Illustration of a man with a beard">
+						</div> <!-- end col -->
+						<div class="col-sm-8">
+							<blockquote>
+								Food truck organic vegan aesthetic, pour-over 8-bit hashtag jean shorts McSweeney's. 90's Echo Park slow-carb polaroid, McSweeney's normcore church-key heirloom tattooed food truck chambray aesthetic single-origin coffee Etsy yr. Tousled American Apparel meditation chia Odd Future, put a bird on it banjo gentrify Portland swag. Shoreditch asymmetrical Neutra, lumbersexual gentrify scenester health goth Kickstarter swag bitters disrupt next level. Cronut selvage retro 90's. Banjo slow-carb Pitchfork, vegan mixtape ugh lumbersexual migas disrupt cred. Actually Vice authentic, ugh Neutra kitsch Etsy.
+								<cite>&mdash; AJ, graduate of this course</cite>
+							</blockquote> <!-- end blockquote -->
+						</div> <!-- end col -->
+					</div> <!-- end row testimonial -->
+
+					<!-- TESTIMONIAL -->
+					<div class="row testimonial">
+						<div class="col-sm-4">
+							<img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/ernest.png" alt="Illustration of a man with a beard in a suit">
+						</div> <!-- end col -->
+						<div class="col-sm-8">
+							<blockquote>
+								Taxidermy cornhole banh mi twee messenger bag forage. Chambray cliche literally ugh, seitan butcher tousled paleo photo booth American Apparel. Blog fixie stumptown, tousled Neutra gluten-free American Apparel listicle occupy irony tilde fap retro Williamsburg kale chips. High Life swag street art, flannel pug yr Wes Anderson. Odd Future wayfarers bespoke, asymmetrical Banksy selvage raw denim pug twee.
+								<cite>&mdash; Ernest, graduate of another course</cite>
+							</blockquote> <!-- end blockquote -->
+						</div> <!-- end col -->
+					</div> <!-- end row testimonial -->
+
+				</div> <!-- end col -->
+			</div> <!-- end row -->
+		</div> <!-- end container -->
+	</section> <!-- end kudos -->
+
+<?php get_footer(); ?>
