75e44947e81db7f148611bbb4d0332599d066d74
Clint Losee <clint.losee@gmail.com>
2015-09-17 10:36:37 -0600

    Code and styling for the archive page template.

--- archive.php
+++ archive.php
@@ -8,44 +8,52 @@
  */
 
 get_header(); ?>
+	<?php if ( have_posts() ) : ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+		<section class="feature-image feature-image-default-alt" data-type="background" data-speed="2">
+			<h1 class="page-title">
+			<?php
+				the_archive_title();
+				the_archive_description( '<small class="taxonomy-description">', '</small>' );
+			?>
+			</h1>
+		</section> <!-- end section -->
 
-		<?php if ( have_posts() ) : ?>
+	<div class="container">
+		<div id="primary" class="row">
+			<main id="content" class="col-sm-8">
 
-			<header class="page-header">
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="taxonomy-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+				<?php /* Start the Loop */ ?>
+				<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+					<?php
 
-				<?php
+						/*
+						 * Include the Post-Format-specific template for the content.
+						 * If you want to override this in a child theme, then include a file
+						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+						 */
+						get_template_part( 'template-parts/content', get_post_format() );
+					?>
 
-					/*
-					 * Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'template-parts/content', get_post_format() );
-				?>
+				<?php endwhile; ?>
 
-			<?php endwhile; ?>
+				<?php the_posts_navigation(); ?>
 
-			<?php the_posts_navigation(); ?>
+			<?php else : ?>
 
-		<?php else : ?>
+				<?php get_template_part( 'template-parts/content', 'none' ); ?>
 
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+			<?php endif; ?>
 
-		<?php endif; ?>
+			</main> <!-- end main content -->
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+			<!-- SIDEBAR
+            ================================ -->
+            <aside class="col-sm-4">
+				<?php get_sidebar(); ?>
+			</aside> <!-- end aside -->
+		</div> <!-- end primary row -->
+	</div> <!-- end container -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
